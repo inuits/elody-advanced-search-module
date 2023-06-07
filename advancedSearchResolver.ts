c76946efc3fd16116ff48f7c21f25f4e4f71912c
Wolf De Rechter <wolfez@inuits.eu>
2023-06-07 11:13:00 +0000

    Update .gitlab-ci.yml, tsconfig.json, advancedSearchResolver.ts

--- advancedSearchResolver.ts
+++ advancedSearchResolver.ts
@@ -1,4 +1,4 @@
-import { Resolvers } from "../../generated-types/type-defs";
+import { Resolvers } from "./generated-types/type-defs";
 import { ContextValue } from "base-graphql";
 
 export const advancedSearchResolver: Resolvers<ContextValue> = {};
